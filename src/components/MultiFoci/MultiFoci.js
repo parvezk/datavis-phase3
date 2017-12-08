6aacdda0737c0c9bcc2f7add6ee31f4e84c54c1b
Parvez Kose <flamboyanz@gmail.com>
2017-12-07 22:10:03 -0500

    update

---
+++ src/components/MultiFoci/MultiFoci.js
@@ -0,0 +1,33 @@
+/*
+import React from "react";
+
+import d3SelectMulti from "d3-selection-multi";
+
+class MultiFoci extends React.Component {
+
+    constructor(props) {
+      super(props);
+      this.state = {};
+    }
+
+    componentWillMount(){
+       
+    }
+
+    componentDidMount() {
+        const { node } = this.props;
+        let ds; //empty var for dataset (ds)
+        //container.append("div")
+        //.attr("class", "bar");
+        console.log(this.props);
+    }
+
+    render() {
+        return(
+            <div>check from MultiFoci</div>
+        );
+    }
+}
+
+export default MultiFoci;
+*/
