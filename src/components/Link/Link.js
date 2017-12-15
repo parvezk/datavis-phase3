789e6531c9233d819ee323309835e7738600cc74
Parvez Kose <flamboyanz@gmail.com>
2017-12-14 19:14:24 -0500

    update

--- src/components/Link/Link.js
+++ src/components/Link/Link.js
@@ -8,8 +8,10 @@ const Link = () => {
     <div className="research_paper">
         
         <blockquote>
-        <h3><a href="http://www.brucelawson.co.uk/2013/on-citing-quotations-again/">Report: How virtual reality will impact journalism</a></h3>
-        <p>Learn how immersive media requires a new reporting approach and how publishers can leverage it by reading “The age of dynamic storytelling: A guide for journalists in a world of immersive 3-D content.”</p>
+        <h3><a href="https://insights.ap.org/industry-trends/report-how-virtual-reality-will-impact-journalism">Report: How virtual reality will impact journalism</a></h3>
+        <p>Learn how immersive media requires a new reporting approach and how publishers can leverage it by reading “The age of dynamic storytelling: A guide for journalists in a world of immersive 3-D content.”
+            Link to the report: <a href="https://insights.ap.org/industry-trends/report-how-virtual-reality-will-impact-journalism" title="Dynamic Storytelling">AP Insights: Age of Dynamic Storytelling</a>
+        </p>
         <footer>
         — <cite><a href="https://insights.ap.org/contributors/francesco-marconi">By Francesco Marconi</a></cite>
         </footer>
